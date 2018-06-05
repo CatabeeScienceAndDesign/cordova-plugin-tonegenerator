11097d3ea2e953b393ecbf58f33edb605b26bd17
Brendan Flynn <brendan@flexvoltbiosensor.com>
2018-06-04 21:33:40 -0400

    Minor - adds license to js bindings, minor lifting

--- www/tonegenerator.js
+++ www/tonegenerator.js
@@ -1,4 +1,32 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Brendan Flynn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
 cordova.define("cordova-plugin-tonegenerator.main", function(require, exports, module) {
+
 var argscheck = require('cordova/argscheck'),
     utils = require('cordova/utils'),
     exec = require('cordova/exec'),
@@ -20,15 +48,15 @@ ToneGenerator.prototype = {
     }, "ToneGenerator", "play", [frequency, volume, waveType]);
   },
   frequency: function(hz) {
-    return cordova.exec(function() {}, function() {throw "Error updating tone"}, "ToneGenerator", "frequency", [hz || 0]);
+    return cordova.exec(function() {}, function() {throw "Error updating tone frequency";}, "ToneGenerator", "frequency", [hz || 0]);
   },
   volume: function(vol) {
     if (vol > 255) vol = 255;
     if (vol < 0) vol = 0;
-    return cordova.exec(function() {}, function() {throw "Error updating tone"}, "ToneGenerator", "volume", [vol || 0]);
+    return cordova.exec(function() {}, function() {throw "Error updating tone volume";}, "ToneGenerator", "volume", [vol || 0]);
   },
   stop: function() {
-    return cordova.exec(function() {}, function() {throw "Error stopping tone"}, "ToneGenerator", "stop", []);
+    return cordova.exec(function() {}, function() {throw "Error stopping tone";}, "ToneGenerator", "stop", []);
   }
 };
 
